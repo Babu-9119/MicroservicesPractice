2b5f96721f8beff9a9a3a0e73cc4a7192ca984ec
Babu-9119 <babureddypyayala@gmail.com>
2026-04-25 22:14:59 +0530

    Created eureka server and make changes to micorservices to register with eurekaserver

---
+++ eurekaserver/src/test/java/com/letsbuild/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letsbuild.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
